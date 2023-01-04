b987109fb98a4d8b4645a3c638ef3384aa4649a9
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-01-04 11:37:44 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "mithileshtiwari-dev-54509",
   url: "https://mithileshtiwari-dev-54509.botics.co",
   api: "https://mithileshtiwari-dev-54509.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,34 @@
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = ({
+  navigation
+}) => {
+  const Login = useSelector(state => state.Login);
+  const Lo = useSelector(state => state.Lo);
+  return <SafeAreaView style={styles.safeArea} onPress={() => navigation.navigation.navigate("undefined", {})}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => navigation.navigation.navigate("undefined", {
+        "Login": Login
+      })}><View style={styles.gqORoVNA}></View></Pressable></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  gqORoVNA: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
+export default Untitled1;

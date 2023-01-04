fdd52e966c794781662342baa5107f0a8047229b
Crowdbotics <engineering@crowdbotics.com>
2023-01-04 13:08:27 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,209 +1,296 @@
-import React, { useState } from "react";
+// @ts-nocheck
+import React, { useEffect, useState } from "react";
 import {
-  Text,
-  StyleSheet,
-  View,
-  SafeAreaView,
-  TextInput,
-  Switch,
-  ScrollView
+    Text,
+    View,
+    StyleSheet,
+    Image,
+    TextInput,
+    Pressable,
+    ScrollView
 } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInfo } from "../../store";
 
-const AccountSettingsScreen = (params) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
-  const [notifications, setNotifications] = useState(false);
-  const [emailNotifications, setEmailNotifications] = useState(false);
-  const [smsNotifications, setSmsNotifications] = useState(false);
-  const [deactivateAccount, setDeactivateAccount] = useState(false);
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.subContainer}>
-          <Text style={styles.subText}>Subscription</Text>
-          <View style={styles.subPallet}>
-            <View style={styles.planDes}>
-              <Text style={[styles.fnt25, styles.boldText]}>Plan</Text>
-              <Text style={styles.fnt16}>Description</Text>
-            </View>
-            <View style={styles.subPricing}>
-              <Text style={[styles.fnt25, styles.boldText]}>$14.99 </Text>
-              <Text style={styles.fnt16}>per month</Text>
-            </View>
-          </View>
+const AccountScreen = () => {
+    const dispatch = useDispatch();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [userAddress, setUserAddress] = useState("");
+
+    const userInfo = useSelector(state => state?.ecommerce?.user);
+
+    const handleGetUser = async () => {
+        await dispatch(getUserInfo()).then((res) => { }).catch((err) => console.log(err))
+    }
+
+    useEffect(() => {
+        if (!userInfo) {
+            handleGetUser()
+        }
+    }, [])
+
+
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.profileContainer}>
+                    <Image source={require("./assets/profilePicture.png")} style={styles.profilePicture} />
+                    <Text style={styles.profileName}>{userInfo?.username}</Text>
+                    <Text style={styles.profilemail}>{userInfo?.email}</Text>
+                </View>
+
+                <View style={[styles.accountHeadings]}>
+                    <Text style={styles.inputText1}>Edit Account</Text>
+                    <Text style={[styles.inputText1, { color: "#EA4335" }]}>Delete Account</Text>
+                </View>
+
+                <View style={styles.inputs}>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputText}>First name</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => setFirstName(text)}
+                            value={userInfo?.first_name}
+                            placeholder="Enter"
+                            placeholderTextColor="#000"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
+
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputText}>Last Name</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => setLastName(text)}
+                            value={userInfo?.last_name}
+                            placeholder="Enter"
+                            placeholderTextColor="#000"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
+
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputText}>Address 1</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => setUserAddress(text)}
+                            placeholder="Enter"
+                            placeholderTextColor="#000"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
+                    </View>
+                </View>
+                <Text style={styles.connectAcc}>Connected accounts</Text>
+                <View style={styles.subHeader}>
+                    <View style={styles.mainContainer}>
+                        <Image
+                            // @ts-ignore
+                            source={require("./assets/btnSigninwithApple.png")}
+                            style={styles.dot}
+                        />
+                        <View>
+                            <View style={styles.orderStatusContainer}>
+                                <Text style={styles.statusHeading}>Apple</Text>
+                                <Text style={[styles.statusHeading, styles.statusTime]}>Connected</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.mainContainer}>
+                        <Image
+                            // @ts-ignore
+                            source={require("./assets/btnSigninwithGoogle.png")}
+                            style={styles.dot}
+                        />
+                        <View>
+                            <View style={styles.orderStatusContainer}>
+                                <Text style={styles.statusHeading}>Google</Text>
+                                <Text style={[styles.statusHeading, styles.statusTime]}>Connected</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={[styles.mainContainer, styles.borderNull]}>
+                        <Image
+                            // @ts-ignore
+                            source={require("./assets/btnSigninwithFacebook.png")}
+                            style={styles.dot}
+                        />
+                        <View>
+                            <View style={styles.orderStatusContainer}>
+                                <Text style={styles.statusHeading}>Facebook</Text>
+                                <Text style={[styles.statusHeading, styles.statusTime]}>Connected</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+
+                <Button buttonText={"Update"} />
+            </ScrollView>
         </View>
-        <View style={styles.billingContainer}>
-          <Text style={styles.billingText}>Billing information</Text>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Full Name</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your full name"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={name}
-              onChangeText={(text) => setName(text)}
-            />
-          </View>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Email Address</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your Email Address"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-            />
-          </View>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Card number</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your Card Number"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={cardNumber}
-              onChangeText={(text) => setCardNumber(text)}
-            />
-          </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    profileContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 15
+    },
+    profilePicture: {
+        width: 82,
+        height: 82,
+        borderRadius: 40
+    },
+    profileName: {
+        fontSize: 20,
+        marginTop: 10
+    },
+    profilemail: {
+        fontSize: 14,
+        color: "grey"
+    },
+    inputs: {
+        marginTop: 20,
+        paddingHorizontal: 20
+    },
+    inputContainer: {
+        flexDirection: "column",
+        justifyContent: "center"
+    },
+    inputText: {
+        fontSize: 13,
+        marginLeft: 20,
+        color: "#111112"
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#e6e6e6",
+        borderRadius: 10,
+        padding: 10,
+        paddingLeft: 20,
+        marginVertical: 10,
+        width: "100%",
+        height: 50,
+        backgroundColor: "#f9f9f9"
+    },
+    lockIcon: {
+        position: "absolute",
+        right: 20,
+        top: 40,
+        width: 20,
+        height: 20,
+        resizeMode: "contain"
+    },
+    bottomTextContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginHorizontal: 20,
+        marginTop: 40
+    },
+    bottomText: {
+        fontSize: 12,
+        textAlign: "center",
+        marginHorizontal: 20
+    },
+    accountHeadings: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        marginHorizontal: 30,
+        paddingVertical: 15,
+        borderBottomColor: "rgba(51, 51, 51, 0.05)",
+        borderBottomWidth: 1,
+        marginTop: 10
+    },
+    inputText1: {
+        fontSize: 15,
+        color: "#12D790"
+    },
+    connectAcc: { fontSize: 14, fontWeight: "bold", color: "#000", paddingHorizontal: 30, marginVertical: 10 },
+    subHeader: {
+        backgroundColor: "#F9F9F9",
+        paddingHorizontal: 30,
+        paddingTop: 10,
+        paddingBottom: 15
+    },
+    mainContainer: {
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        paddingHorizontal: 10,
+        alignItems: "center",
+        marginTop: 10,
+        borderBottomColor: "rgba(51, 51, 51, 0.05)",
+        borderBottomWidth: 1,
+        paddingBottom: 10
+    },
+    borderNull: { borderBottomWidth: 0 },
+    dot: { height: 16, width: 16, resizeMode: "contain", marginLeft: -10 },
+    orderStatusContainer: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingLeft: 10,
+        paddingRight: 8,
+    },
+    statusHeading: { fontSize: 14, width: "50%", color: "#222222" },
+    statusText: { paddingHorizontal: 10, fontSize: 10, color: "#2A2B2E" },
+    statusTime: { textAlign: 'right', color: "#12D790", fontWeight: "bold" },
+});
+
+export default AccountScreen;
+
+
+
+const Button = (params) => {
+    const btnStyle = {
+        backgroundColor: params.outline ? "#fff" : "#000",
+        borderColor: params.outline ? "#000" : "#fff",
+        borderWidth: 1
+    };
+    const btnText = {
+        color: params.outline ? "#000" : "#fff"
+    };
+    return (
+        <View style={buttonStyles.btnContainer}>
+            <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
+                <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
+                <View style={buttonStyles.childrenContainer}>{params.children}</View>
+            </Pressable>
         </View>
-        <View style={styles.togglesContainer}>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={notifications}
-              onValueChange={(value) => setNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Email Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={emailNotifications}
-              onValueChange={(value) => setEmailNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>SMS Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={smsNotifications}
-              onValueChange={(value) => setSmsNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Deactivate Account</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={deactivateAccount}
-              onValueChange={(value) => setDeactivateAccount(value)}
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  subContainer: {
-    paddingHorizontal: 20,
-    flex: 0.2,
-    justifyContent: "center"
-  },
-  subText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
-  },
-  subPallet: {
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    padding: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  planDes: {
-    flex: 0.4,
-    padding: 10,
-    // borderWidth: 1,
-    // borderColor: '#979797',
-    justifyContent: "center",
-    alignItems: "flex-start"
-  },
-  subPricing: {
-    flex: 0.6,
-    padding: 10,
-    // borderWidth: 1,
-    // borderColor: '#979797',
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "center"
-  },
-  boldText: {
-    fontWeight: "bold"
-  },
-  fnt25: {
-    fontSize: 25
-  },
-  fnt16: {
-    fontSize: 16
-  },
-  billingContainer: {
-    flex: 0.5,
-    paddingHorizontal: 20 // borderWidth: 1,
-    // borderColor: '#979797',
-  },
-  billingText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
-  },
-  inputText: {
-    fontSize: 16,
-    marginLeft: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    borderRadius: 10,
-    padding: 10,
-    paddingLeft: 20,
-    marginVertical: 10,
-    width: "100%"
-  },
-  togglesContainer: {
-    flex: 0.3,
-    paddingHorizontal: 20
-  },
-  toggle: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  toggleText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
-  }
+const buttonStyles = StyleSheet.create({
+    btnContainer: {
+        paddingHorizontal: 40,
+        justifyContent: "center",
+        marginVertical: 20
+    },
+    btn: {
+        backgroundColor: "black",
+        height: 50,
+        width: "100%",
+        padding: 10,
+        paddingHorizontal: 25,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: "rgba(0, 0, 0, 0.2)",
+        elevation: 10,
+        flexDirection: "row"
+    },
+    btnText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    childrenContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+    }
 });
-export default AccountSettingsScreen;

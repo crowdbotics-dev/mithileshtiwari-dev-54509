82ed4a77a986d0fa34667c66a68d7de7535a53bd
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-02-02 12:36:35 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled15/index.js
+++ screens/Untitled15/index.js
@@ -1,28 +1,20 @@
-import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled12 = () => {
+const Untitled15 = () => {
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.WmcplpkO}></View></ScrollView>
+    }}></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
-  },
-  WmcplpkO: {
-    height: 60,
-    width: 140,
-    backgroundColor: "#E4E4E4",
-    borderRadius: 0,
-    color: "#777777"
   }
 });
-export default Untitled12;
+export default Untitled15;
